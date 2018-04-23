bda6f94269d7ff1d28ec8b15b8fa9e2475918645
Raman Hundal <rshundal11@gmail.com>
2018-04-23 10:51:51 +0000

    Added http methods to states object

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 '''view for State'''
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, request
 from models import storage
+import json
 from models.state import State
 
 states = Blueprint('states', __name__)
+
 
 @states.route('/states', methods=['GET'])
 def all_states():
@@ -16,3 +18,50 @@ def all_states():
         state_dict = state.to_dict()
         states_list.append(state_dict)
     return jsonify(states_list)
+
+@states.route('/states', methods=['POST'])
+def post_dict():
+    '''transforms http body request to a dictionary'''
+    try:
+        data = request.get_json()
+    except:
+        return jsonify("Not a JSON"), 400
+    if 'name' in data:
+        new_state = State(**data)
+        new_state.save()
+        return jsonify(new_state.to_dict()), 201
+    else:
+        return jsonify("Missing name"), 400
+
+
+@states.route('/states/<state_id>', methods=['GET', 'DELETE'])
+def retrieve_state(state_id):
+    ''' retrieves state if not linked to object'''
+    value = storage.get('State', state_id)
+    if request.method == 'DELETE':
+        if value is None:
+            abort(404)
+        state_storage = storage.all('State')
+        for state in state_storage.values():
+            if state.id == state_id:
+                storage.delete(state)
+        return jsonify({}), 200
+    if value is None:
+        abort(404)
+
+@states.route('/states/<state_id>', methods=['PUT'])
+def update_state(state_id):
+    '''updates state object'''
+    state_obj = storage.get('State', state_id)
+    if state_obj is None:
+        abort(404)
+    try:
+        data = request.get_json()
+    except:
+        return jsonify('Not a JSON'), 400
+    for key, value in data.items():
+        if key != 'id' or key != 'created_at' or key != 'updated_at':
+            setattr(state_obj, key, value)
+    state_obj.save()
+    updated_state = state_obj.to_dict()
+    return (jsonify(updated_state)), 200
