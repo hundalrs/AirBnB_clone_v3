fb4c51e78570b42003a283b3d342dbcd96db60c8
Raman Hundal <rshundal11@gmail.com>
2018-04-23 13:42:56 +0000

    Added new view for Place objects that handles all default RestFul API actions

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 
 from flask import Flask, jsonify
 from models import storage
-from api.v1.views import app_views, states, cities, amenities, users
+from api.v1.views import app_views, states, cities, amenities, users, places
 from os import getenv
 
 app = Flask(__name__)
@@ -13,6 +13,7 @@ app.register_blueprint(states, url_prefix='/api/v1')
 app.register_blueprint(cities, url_prefix='/api/v1')
 app.register_blueprint(amenities, url_prefix='/api/v1')
 app.register_blueprint(users, url_prefix='/api/v1')
+app.register_blueprint(places, url_prefix='/api/v1')
 
 @app.teardown_appcontext
 def storage_closer(exceptions):
