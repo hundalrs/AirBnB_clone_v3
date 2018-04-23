65fd997c8d173eb80b03d5cb8634549fdd498cea
Raman Hundal <rshundal11@gmail.com>
2018-04-23 10:51:11 +0000

    Added mapping to strict slashes

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,6 +7,7 @@ from api.v1.views import app_views, states
 from os import getenv
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views, url_prefix='/api/v1')
 app.register_blueprint(states, url_prefix='/api/v1')
 
