b19b645c8de4461e1847319fe7a3f97d8625d316
squidcarroll <squidcarroll@gmail.com>
2018-04-24 07:47:27 +0000

    add http access control

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,8 +5,10 @@ from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views, states, cities, amenities, users, places, reviews
 from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, origins='0.0.0.0')
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views, url_prefix='/api/v1')
 app.register_blueprint(states, url_prefix='/api/v1')
